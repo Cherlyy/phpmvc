c50b707d62aada04458d671073008ae7435cfe7c
Cherly Cheong <cherlycheong@gmail.com>
2019-10-07 01:01:15 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,20 @@
 <?php
 
-class About {
+class About extends Controller {
 	public function index($name = 'Cherly', $occupation = 'student')
 	{
-		echo "Hello, $name is a $occupation.";
+		$data['name'] = $name;
+		$data['occupation'] = $occupation;
+		$data['title'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 	public function page()
 	{
-		echo 'About/page';
+		$data['title'] = 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
---
+++ app/view/about/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman About</title>
+</head>
+<body>
+	<h1>About Me</h1>
+	<p>Hello, my name is <?= $data['name'];  ?>, a <?= $data['occupation'];  ?> .</p>
+</body>
+</html>
